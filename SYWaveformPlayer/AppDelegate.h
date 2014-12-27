eb9dfcb16b98b17a25f3b7fc558bfce0bea35613
Spencer Yen <spencersyen@gmail.com>
2014-12-26 23:28:45 -0800

    Initial Commit

---
+++ SYWaveformPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYWaveformPlayer
+//
+//  Created by Spencer Yen on 12/26/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYWaveformPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYWaveformPlayer
+//
+//  Created by Spencer Yen on 12/26/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
